bc32704aa7cb9fe5245868e53ef5dadb573528e2
Cry <m.f.zuravlev@yandex.ru>
2023-02-16 15:33:42 +0500

    добавил ветку исключения 404

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,12 @@ fn bootstrap(mut stream: TcpStream) {
 
         stream.write_all(response.as_bytes()).unwrap();
     } else {
-        // какой-то другой запрос
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
