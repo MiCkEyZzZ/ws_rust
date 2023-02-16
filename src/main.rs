ec0b4e83650c5ae1dc70cca735ef9fc783721aea
Cry <m.f.zuravlev@yandex.ru>
2023-02-16 15:23:36 +0500

    добавил проверку запоса и возврат

--- src/main.rs
+++ src/main.rs
@@ -16,17 +16,17 @@ fn main() {
 
 fn bootstrap(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|r| r.unwrap())
-        .take_while(|l| !l.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        // какой-то другой запрос
+    }
 }
